60b43f71a4da2ffdd055aafa26a56551bbe53611
Aditya <adityagundg14@gmail.com>
2025-07-14 15:07:09 +0530

    Create LiveShow Model

--- bookMyShow/aditya/src/main/java/com/aditya/Model/LiveShow.java
+++ bookMyShow/aditya/src/main/java/com/aditya/Model/LiveShow.java
@@ -1,5 +1,39 @@
 package com.aditya.Model;
 
-public class LiveShow {
+import java.util.HashMap;
+import java.util.Map;
 
+import com.aditya.constant.Genre;
+import com.aditya.constant.TimeSlot;
+
+public class LiveShow {
+	private String name;
+    private Genre genre;
+    private Map<TimeSlot, ShowSlot> showSlots;
+    
+    public LiveShow(String name, Genre genre) {
+        this.name = name;
+        this.genre = genre;
+        this.showSlots = new HashMap<>();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public Map<TimeSlot, ShowSlot> getShowSlots() {
+        return showSlots;
+    }
+
+    public void addSlot(TimeSlot slot, ShowSlot showSlot) {
+        this.showSlots.put(slot, showSlot);
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + genre;
+    }
 }
